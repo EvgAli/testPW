a25ef77afe54f9438c1af832ad7916bc8eecf454
Evgeny Aliev <Unafreid@gmail.com>
2026-04-23 10:23:48 +0400

    allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   testDir: "./tests",
   use: {
     headless: true,
+    // Добавляем для Allure полезные вложения при падении
+    screenshot: "only-on-failure",
+    trace: "on-first-retry",
+    video: "retain-on-failure",
   },
+  // 👇 Добавляем Allure репортер
+  reporter: [
+    ["html"], // стандартный отчёт Playwright (опционально)
+    ["allure-playwright"], // Allure отчёт
+  ],
 });
